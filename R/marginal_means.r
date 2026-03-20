6015766c5a70d056b54077ea5d9bdf90c1aba3fa
kssrr <lennart.kasserra@gmail.com>
2026-03-20 21:04:31 +0100

    autoplot

--- R/marginal_means.r
+++ R/marginal_means.r
@@ -1,58 +1,53 @@
 # Marginal means, conditional marginal means, and autoplot-methods for marginal means.
 
 #' @export
-marginal_means <- function(
-  data, 
-  formula = NULL, 
-  outcome = NULL, 
-  attributes = NULL, 
-  cluster
-) {
+marginal_means <- function(data, formula = NULL, outcome = NULL, attributes = NULL, id) {
   
   if (!is.null(formula)) {
-    
-    outcome <- deparse(rlang::f_lhs(formula))
+    outcome    <- deparse(rlang::f_lhs(formula))
     attributes <- labels(terms(formula))
-    
   }
   
   mods <- attributes |> lapply(function(x) {
     lm(reformulate(x, response = outcome, intercept = FALSE), data = data)
   })
   
-  mods |>
-    lapply(function(model) {
+  results <- 
+    Map(function(model, attr) {
       
-      model |>
-        coeftest(vcov. = vcovCL(model, cluster = cluster, type = "HC1")) |>
+      tidy_mod <- model |>
+        lmtest::coeftest(vcov. = sandwich::vcovCL(model, cluster = id)) |>
         broom::tidy()
       
-    }) |>
-    do.call(rbind, args = _) |> 
-    _[, c("term", "estimate", "std.error")]
+      tidy_mod$attribute <- attr
+      tidy_mod$level     <- model$xlevels[[attr]]
+      tidy_mod
+      
+    }, mods, attributes) |>
+    do.call(rbind, args = _) |>
+    _[, c("attribute", "level", "term", "estimate", "std.error")]
+  
+  class(results) <- c("marginal_means", class(results))
+  
+  results
   
 }
 
 #' @export
-conditional_marginal_means <- function(df, formula, group, cluster) {
+autoplot.marginal_means <- function(df) {
   
   df |> 
-    dplyr::group_by({{ group }}) |> 
-    tidyr::nest() |> 
     dplyr::mutate(
-      mms = lapply(data, function(d) {
-        
-        marginal_means(
-          d,
-          formula,
-          cluster = cluster
-        )
-        
-      })
+      lower = estimate - std.error,
+      upper = estimate + std.error,
+      label = paste0(stringr::str_to_title(attribute), ": ", level)
     ) |> 
-    dplyr::select({{ group }}, mms) |> 
-    tidyr::unnest(mms) |> 
-    dplyr::ungroup()
+    ggplot2::ggplot(ggplot2::aes(x = estimate, y = label, color = attribute)) +
+    ggplot2::geom_vline(xintercept = .5, lty = "dotted") +
+    ggplot2::geom_point() +
+    ggplot2::geom_linerange(ggplot2::aes(xmin = lower, xmax = upper)) +
+    ggplot2::theme(legend.position = "none") +
+    ggplot2::labs(x = "Marginal Mean", y = "")
   
 }
 
