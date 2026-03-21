903729fb52f56049704c9ff7bdc1ebb449bd327e
kssrr <lennart.kasserra@gmail.com>
2026-03-21 12:08:28 +0100

    AMCEs auch angepasst

--- R/amce.r
+++ R/amce.r
@@ -26,44 +26,75 @@
 #' amce(data, outcome = "selected", attributes = c("group", "sex", "age"), id = ~uuid)
 #'
 #' @export
-amce <- function(data, formula = NULL, outcome = NULL, attributes = NULL, id) {
+amce <- function(data, formula = NULL, outcome = NULL, attributes = NULL, id = NULL, vcov_type = "HC1") {
   
   if (!is.null(formula)) {
     outcome    <- deparse(rlang::f_lhs(formula))
     attributes <- labels(terms(formula))
   }
   
+  validate_inputs(data, attributes, id)
+  
+  # fit models separately (could be one model but this
+  # makes processing a bit more streamlined):
+  
   mods <- attributes |> lapply(function(x) {
     lm(reformulate(x, response = outcome), data = data)
   })
   
   results <- 
-    Map(function(model, attr) {
+    purrr::map2(mods, attributes, function(model, attr) {
     
-    tidy_mod <- model |>
-      lmtest::coeftest(vcov. = sandwich::vcovCL(model, cluster = id)) |>
-      broom::tidy()
+      # cluster SEs if `id` was provided:
+      
+      if (!is.null(id)) {
+        
+        processed <- lmtest::coeftest(
+          model,
+          vcov. = sandwich::vcovCL(
+            model,
+            cluster = id,
+            type = vcov_type
+          )
+        )
+        
+      } else {
+        
+        processed <- lmtest::coeftest(model)
+        
+      }
+      
+      # the first factor level is the baseline & does not appear
+      # in the model results; adding baselines back in (we esp.
+      # want these to show up in the plots):
+      
+      all_levels     <- model$xlevels[[attr]]
+      baseline_level <- all_levels[1]
+      
+      baseline_row <- data.frame(
+        term      = paste0(attr, baseline_level),
+        estimate  = 0,
+        std.error = 0,
+        statistic = 0,
+        p.value = NA_real_
+      )
+      
+      # put everything together & tidy up:
+      
+      processed |> 
+        broom::tidy() |> 
+        dplyr::filter(term != "(Intercept)") |> 
+        dplyr::add_row(baseline_row, .before = 1) |> 
+        #^ have to *pre*pend this one for row order to match:
+        dplyr::mutate(attribute = attr, level = all_levels)
     
-    all_levels     <- model$xlevels[[attr]]
-    baseline_level <- all_levels[1]
-    
-    baseline_row <- data.frame(
-      term      = paste0(attr, baseline_level),
-      estimate  = 0,
-      std.error = NA_real_,
-      statistic = NA_real_,
-      p.value = NA_real_
+    }) |> 
+    dplyr::bind_rows() |> 
+    dplyr::mutate(lower = estimate - std.error, upper = estimate + std.error) |> 
+    dplyr::select(
+      term, estimate, std.error, lower, upper,
+      statistic, p.value, attribute, level
     )
-    
-    tidy_mod <- tidy_mod[tidy_mod$term != "(Intercept)", ]
-    
-    result           <- rbind(baseline_row, tidy_mod)
-    result$attribute <- attr
-    result$level     <- all_levels
-    
-    result
-    
-  }, mods, attributes) |> do.call(rbind, args = _)
   
   class(results) <- c("amce", class(results))
   
@@ -93,16 +124,23 @@ autoplot.amce <- function(df) {
   
   df |> 
     dplyr::mutate(
-      lower = estimate - std.error,
-      upper = estimate + std.error,
-      label = paste0(attribute, ": ", level)
+      label = paste0(attribute, ": ", level),
+      sig = dplyr::case_when(
+        p.value < .01 ~ "***",
+        p.value < .05 ~ "**",
+        p.value < .1 ~ "*",
+        TRUE ~ ""
+      )
     ) |> 
     ggplot2::ggplot(ggplot2::aes(x = estimate, y = label, color = attribute)) +
     ggplot2::geom_vline(xintercept = 0, lty = "dotted") +
     ggplot2::geom_point() +
+    ggplot2::geom_text(
+      ggplot2::aes(label = sig),
+      position = ggplot2::position_nudge(y = .15)
+    ) +
     ggplot2::geom_linerange(ggplot2::aes(xmin = lower, xmax = upper)) +
     ggplot2::theme(legend.position = "none") +
     ggplot2::labs(x = "AMCE", y = "")
   
 }
-
